997c02d138cb3fd64a937313570cc72e24cf6e0e
Alvaro Segura <alvarosg5@gmail.com>
2026-01-29 15:39:01 +0100

    [tests] Extending the coverage to a 100%

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -6,3 +6,43 @@ def test_foo():
     gilded_rose = GildedRose([item])
     gilded_rose.update_quality()
     assert item.name == "foo"
+
+
+def test_quality_greater_than_zero():
+    item = Item("foo", 0, 1)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.name == "foo"
+
+
+def test_backstage_quality_increases():
+    item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 43
+
+
+def test_sell_in_less_than_zero():
+    item = Item("foo", -1, 2)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 0
+
+
+def test_backstage_sell_in_less_than_zero():
+    item = Item("Backstage passes to a TAFKAL80ETC concert", -1, 1)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 0
+
+
+def test_aged_brie_sell_in_less_than_zero():
+    item = Item("Aged Brie", -1, 1)
+    gilded_rose = GildedRose([item])
+    gilded_rose.update_quality()
+    assert item.quality == 3
+
+
+def test_representation_item():
+    item = Item("foo", -1, 2)
+    assert str(item) == "foo, -1, 2"
