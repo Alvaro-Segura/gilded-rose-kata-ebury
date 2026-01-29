c386e54b8a9dd9af2c9258151ed9c88b6587e21e
Daniel García C <daniel.garcia@ebury.com>
2026-01-29 16:19:58 +0100

    {Refractor}: Moved logic to functions based on behaviour

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,34 +7,13 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            match item.name:
+                case "Aged Brie":
+                    update_aged_brie(item)
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    update_backstage_pass(item)
+                case _:
+                    update_general(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -44,3 +23,26 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+def update_aged_brie(item):
+    item.quality = min(item.quality + 1, 50)
+    item.sell_in -= 1
+    if item.sell_in < 0 :
+        item.quality = min(item.quality + 1, 50)
+
+def update_general(item):
+    if item.sell_in > 0:
+        item.quality = min(item.quality - 1, 0)
+    else:
+        item.quality = min(item.quality - 2, 0)
+    item.sell_in -= 1
+
+def update_backstage_pass(item):
+    if item.sell_in < 0:
+        item.quality = 0
+    elif item.sell_in <= 5: 
+        item.quality += 3
+    elif item.sell_in <= 10: 
+        item.quality += 2
+    else:
+        item.quality += 1
